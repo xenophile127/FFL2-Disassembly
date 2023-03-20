edee58605203897e8d550320b49e8f65dd69e3a5
Daid <daid303@gmail.com>
2023-03-20 15:36:52 +0100

    Update gfx

--- src/bank02.asm
+++ src/bank02.asm
@@ -7,20 +7,20 @@ INCLUDE "include/constants.inc"
 
 SECTION "bank02", ROMX[$4000], BANK[$02]
 
-;@gfximg name=tiles1 width=64 height=2
+;@gfximg name=tiles1 width=2 height=64
 data_02_4000:
     INCBIN "tiles1.bin"                                ;; 02:4000
-;@gfximg name=tiles2 width=64 height=2
+;@gfximg name=tiles2 width=2 height=64
     INCBIN "tiles2.bin"                                ;; 02:4800
-;@gfximg name=tiles3 width=64 height=2
+;@gfximg name=tiles3 width=2 height=64
     INCBIN "tiles3.bin"                                ;; 02:5000
-;@gfximg name=tiles4 width=64 height=2
+;@gfximg name=tiles4 width=2 height=64
     INCBIN "tiles4.bin"                                ;; 02:5800
-;@gfximg name=tiles5 width=64 height=2
+;@gfximg name=tiles5 width=2 height=64
     INCBIN "tiles5.bin"                                ;; 02:6000
-;@gfximg name=tiles6 width=64 height=2
+;@gfximg name=tiles6 width=2 height=64
     INCBIN "tiles6.bin"                                ;; 02:6800
-;@gfximg name=tiles7 width=64 height=2
+;@gfximg name=tiles7 width=2 height=64
     INCBIN "tiles7.bin"                                ;; 02:7000
-;@gfximg name=tiles8 width=64 height=2
+;@gfximg name=tiles8 width=2 height=64
     INCBIN "tiles8.bin"                                ;; 02:7800
